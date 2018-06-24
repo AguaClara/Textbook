d1564a3cfd66b45f2371be6f686c5c100732490d
ethan92429 <ethan.keller@gmail.com>
2018-06-24 10:16:07 -0400

    added .nojekyll so it gets copied over to the output.

--- conf.py
+++ conf.py
@@ -71,6 +71,7 @@ language = None
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
 exclude_patterns = []
+html_extra_path = [".nojekyll"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
