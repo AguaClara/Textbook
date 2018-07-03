37e4bf5f2ec72b206e204e3fe1a806d766a9dfed
juanchon998 <jfg98@cornell.edu>
2018-07-03 15:48:14 -0400

    ahhhhhh nothing works anymore

--- Flow_Control_and_Measurement/matplotlib_test.py
+++ Flow_Control_and_Measurement/matplotlib_test.py
@@ -1,13 +1,17 @@
-from aide_design.play import *
-
+#from aide_design.play import *
 # Create a list of 100 numbers between 0 and 100 and then assign the units of degC to the array.
 # This array will be the x values of the graph.
 
-x = np.arange(2) * u.degC
-GraphTarray = u.Quantity(np.arange(100),u.degC)
+#x = np.arange(2) * u.degC
+#GraphTarray = u.Quantity(np.arange(100),u.degC)
 
 #Note the use of the .to method below to display the results in a particular set of units.
-plt.plot(GraphTarray, pc.viscosity_kinematic(GraphTarray).to(u.mm**2/u.s), '-')
+#plt.plot(GraphTarray, pc.viscosity_kinematic(GraphTarray).to(u.mm**2/u.s), '-')
 #plt.xlabel('Temperature (degrees Celcius)')
 #plt.ylabel('Viscosity (mm^2/s)')
 #plt.show()
+
+
+import sys
+from PyQt5 import QtWidgets
+app = QtWidgets.QApplication(sys.argv)
