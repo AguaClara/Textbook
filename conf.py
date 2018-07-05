05dc9b3f8cf8c821f3135976d4c5c9467df17caf
juanchon998 <jfg98@cornell.edu>
2018-07-05 10:12:07 -0400

    reverting conf.py to original state. We need to find another way to incorporate the matplotlib functionality

--- conf.py
+++ conf.py
@@ -32,8 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.doctest', 'sphinxcontrib.disqus',
-    'sphinx.ext.mathjax', 'matplotlib.sphinxext.only_directives', 
-    'matplotlib.sphinxext.plot_directive']
+    'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
