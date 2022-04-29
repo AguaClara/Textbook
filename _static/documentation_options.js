3c8512c76e0e8fcaa16d667610c71fd7b6380969
fletchapin <fletchapin@users.noreply.github.com>
2022-04-29 05:49:40 +0000

    Deploying to test from  @ 20b91b7ae63d9d41772435f77d67f15a6f32c192 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '223/merge',
+    VERSION: '240/merge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
