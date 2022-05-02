157c0147925215005d4bab58477801c71bf8ba5d
Monroe Weber-Shirk <monroews@gmail.com>
2022-05-02 10:43:19 -0400

    AIDE now links to current Smart Part version

---
+++ AIDE/About/about.cmd
@@ -0,0 +1,8 @@
+pandoc -V geometry:margin=1in Clarifier.rst -o Clarifier.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in ET.rst -o ET.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in FlocHH.rst -o FlocHH.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in FlocHV.rst -o FlocHV.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in FLocVH.rst -o FlocVH.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in LFOM.rst -o LFOM.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in OStaRS.rst -o OStaRS.pdf --pdf-engine=xelatex
+pandoc -V geometry:margin=1in Plant20-80.rst -o Plant20-80.pdf --pdf-engine=xelatex
