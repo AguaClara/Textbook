1cbf18f5f94491a5f9f88c126b1637184b8d82c6
monroews <monroews@users.noreply.github.com>
2022-05-05 00:38:23 +0000

    Deploying to test from  @ 3dbe52a5f685831e5a5a5140125bb26e9a38aedf 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '305/merge',
+    VERSION: '306/merge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
