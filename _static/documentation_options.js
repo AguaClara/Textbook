f6e98d854b1734353abbfb893106479ba7636a2b
monroews <monroews@gmail.com>
2022-05-16 21:06:41 +0000

    Deploying to gh-pages from master @ 878431349e552f186e33f15c2334506a9f817a99 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.3.37',
+    VERSION: 'v1.3.38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
