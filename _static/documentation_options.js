a84d014362eefa777845a2c0ec58928f530185c1
fletchapin <fletchapin@users.noreply.github.com>
2022-05-17 18:11:08 +0000

    Deploying to test from @ AguaClara/Textbook@7b40573d055a669eca265d2469951e998ea5f580 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '322/merge',
+    VERSION: '324/merge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
