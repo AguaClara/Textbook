68e58c577c2cced43d62de3f349b6c3f86ef6242
monroews <monroews@users.noreply.github.com>
2022-10-04 16:42:50 +0000

    Deploying to test from @ AguaClara/Textbook@23f4fda92098587e38453c8ce4af72ec8004f195 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '339/merge',
+    VERSION: '340/merge',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
