80719efb2940f195852336ff59319155c2ff9ab6
fletchapin <fletchapin@gmail.com>
2023-02-08 06:45:14 +0000

    Deploying to gh-pages from @ AguaClara/Textbook@c1d5d175f9d8486cb4651ef16d28b982558c3b73 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.4.15',
+    VERSION: 'v1.4.16',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
