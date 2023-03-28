6a99205ad479b7f79ba0e789d41452d2dc43bad6
clare45 <31296324+clare45@users.noreply.github.com>
2023-03-28 20:52:45 +0000

    Deploying to gh-pages from @ AguaClara/Textbook@125911c5d90a96a62293dbfb9fcb41ab94514d3f 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.4.23',
+    VERSION: 'v1.4.24',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
