3812f3bba3d4aeb4ad38bef66b7aefaa18afbf87
Monroe Weber-Shirk <monroews@gmail.com>
2023-09-11 07:17:57 -0400

    explore using ! as special character to indicate a variable name

--- _ext/keyword_replacer.py
+++ _ext/keyword_replacer.py
@@ -1,10 +1,11 @@
 from docutils import nodes
 from docutils.parsers.rst import Directive, roles
 from sphinx.util.docutils import SphinxDirective
-
+# we need some way of clearly designating that this string is to be replaced
+# and isn't part of the standard text.
 keyword_map = {
-    "KEYWORD1": "Replacement1",
-    "KEYWORD2": "Replacement2",
+    "!KEYWORD1!": "Replacement1",
+    "!KEYWORD2!": "Replacement2",
     # ... add as many as you want
 }
 
